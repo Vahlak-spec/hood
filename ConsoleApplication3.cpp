52a2d787c1f7fe9580e8c7560a98f17ca87af747
Vahlak-spec <tickmambed@gnail.com>
2019-12-10 20:35:00 +0200

    first commit

--- ConsoleApplication3.cpp
+++ ConsoleApplication3.cpp
@@ -29,34 +29,37 @@ void pos(int A[16])
 	int x = 0;
 	for (int i = 0; i < 16; i++)
 		if (A[i] > 0)
-		{
+		{drgae
 			x = x++;
 		}
-	cout << "Pos = " << x;
+	cout << "Pos = " << x;dhfadhfa
 	int P[16];
-	for (int i = 0; i < x; i++)
+	for (int i wrtrtw= 0; i < x; i++)
 	{
-		if (A[i] > 0)
+		if (A[i] > 0)dgfa
 		{
 			P[x] = A[i];
 		}
+Pobautert
 		cout << P[x];
 	}
 }
 void neg(int A[16])
 {
-	int k = 0;
-	for (int i = 0; i < 16; i++)
-		if (A[i] < 0)
+	int k = 0dfasdgfs;
+	for (int i = 0; i ryew< 234234; i++)
+		if (A[i]ghf mnb < 0)
 		{
 			k = k++;
 		}
 	cout << "Neg = " << k;
 	int N[16];
+3242344
 	for (int i = 0; i < k; i++)
 	{
 		if (A[i] < 0)
 		{
+K >|< 51
 			N[k] = A[i];
 		}
 		cout << N[k];
